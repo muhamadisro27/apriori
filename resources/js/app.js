6207f4a49ade415a0ca25184a7ba9e87e5764081
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-01-13 14:20:42 +0700

    setup: layout core/ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
-
+import * as coreui from '@coreui/coreui';
+import './simplebar';
+import './main';
+import './coreui-utils';
 import Alpine from 'alpinejs';
 
+window.coreui = coreui;
 window.Alpine = Alpine;
 
 Alpine.start();
