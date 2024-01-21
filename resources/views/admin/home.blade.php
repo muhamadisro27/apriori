764c6c4740104225a0a7f114028cb17cec493aa0
muhamadisro27 <isro.sabanur@trans-cosmos@co.id>
2024-01-21 13:41:46 +0700

    feat: add text

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -12,4 +12,14 @@
     </x-slot>
 
 
+    <div class="px-3 body flex-grow-1">
+        <div class="container-lg text-center">
+            <h3>Selamat datang di APLIKASI APRIORI</h3>
+            <h5 class="mt-5">Angga Jaya Store</h5>
+            <img width="450" class="mt-5" src="{{ asset('images/default/91773395-e778-4294-a29f-3ed9c4d54ad2 1.png') }}" alt="logo">
+        </div>
+    </div>
+
+
+
 </x-app-layout>
